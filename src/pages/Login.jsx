b1817b7992e3bcc21e5dc61806bcb57c70b9b9ff
umutplvn <umutpehlivan2078@gmail.com>
2024-01-29 03:31:59 +0000

    up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import AuthForm from "../components/AuthForm";
+import { Box, Typography } from "@mui/material";
 
 
 const Login = () => {
 
   return (
-      <AuthForm/>
+    <>
+    <AuthForm/>
+    <Box sx={{position:"fixed", bottom:"0", backgroundColor:"red", border:"5px", borderRadius:"1rem", padding:"0.5rem"}}>
+    <Typography sx={{fontSize:"12px", color:"white"}}>Username: panel</Typography>
+    <Typography  sx={{fontSize:"12px",  color:"white"}}>Email: panel@gmail.com</Typography>
+    <Typography  sx={{fontSize:"12px",  color:"white"}}>Password: 1a2b3c4D.</Typography>
+    </Box>
+    </>
   );
 };
 
